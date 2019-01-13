c0296a33612f6c0e883e7d723ce392b03e8ef19f
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-01-13 04:55:54 +0000

    Add a API to get the list of the contests

--- src/atcoder.ts
+++ src/atcoder.ts
@@ -2,18 +2,36 @@ import * as cheerio from "cheerio"
 import { HttpClient, IClient } from "./client"
 import { Session } from "./session"
 
+export interface IUrl {
+    atcoder?: string
+    atcoderProblems?: string
+}
+
 export class AtCoder {
+    private url: { atcoder: string, atcoderProblems: string }
     constructor(private session: Session, private client: IClient = new HttpClient(),
-                private domain: string = "https://atcoder.jp") {}
+                url?: IUrl) {
+        url = url || {}
+        this.url = {
+            atcoder: url.atcoder || "https://atcoder.jp",
+            atcoderProblems: url.atcoderProblems || "https://kenkoooo.com/atcoder",
+        }
+    }
     public async login(username: string, password: string): Promise<void> {
-        const r = await this.client.get(`${this.domain}/login`, { session: this.session })
+        const r = await this.client.get(`${this.url.atcoder}/login`, { session: this.session })
         const dom = cheerio.load(r.body)
         const csrfToken = dom(`input[name=csrf_token]`).attr("value")
-        const r2 = await this.client.postForm(`${this.domain}/login`,
+        const r2 = await this.client.postForm(`${this.url.atcoder}/login`,
             { username, password, csrf_token: csrfToken }, { session: this.session })
 
         if (r2.body !== "") {
             throw new Error(`Login failed: ${r2.body}`)
         }
+    }
+    public async contests(): Promise<string[]> {
+        const response = await this.client.get(`${this.url.atcoderProblems}/resources/contests.json`,
+                                               { session: new Session() })
+        const contests = JSON.parse(response.body.toString())
+        return contests.map((data: { id: string }) => data.id)
     }
 }
