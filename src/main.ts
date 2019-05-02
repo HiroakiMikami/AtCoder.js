a526dc382108a200433fd85bdf1f6061ee484932
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-05-02 01:50:01 +0000

    Export Language enum

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
-import { AtCoder } from "./atcoder"
+import { AtCoder, Language } from "./atcoder"
 import * as Client from "./client"
 import { Session } from "./session"
 
 export {
     AtCoder,
     Client,
+    Language,
     Session,
 }
